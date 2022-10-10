d39309a4163db4a0ffa20364bb07eefa86eae519
Justin <contactjustinzamora@gmail.com>
2022-10-09 23:12:59 -0700

    webpack loader integration

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,25 @@ const path = require('path');
 module.exports = {
   entry: './lib/index.js',
   output: {
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, 'lib'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      }
+    ]
+  }
 };
